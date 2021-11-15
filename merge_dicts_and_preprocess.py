b0864f27b2dade006ee2c2be9e1fcb1d337b63c3
bigfarts <90592158+bigfarts@users.noreply.github.com>
2021-11-14 17:23:49 -0800

    Remerge dictionaries

--- merge_dicts_and_preprocess.py
+++ merge_dicts_and_preprocess.py
@@ -22,18 +22,23 @@ def replace_diacritics(reading):
 
 for fn in os.listdir('dictionaries'):
     source, ext = os.path.splitext(fn)
+    if ext != '.ndjson':
+        continue
 
     with open(os.path.join('dictionaries', fn), 'r') as f:
-        for line in f:
-            l = json.loads(line)
-            json.dump({
-                'word': l['word'],
-                'simplified': [occ.convert(l['word'])],
-                'source': source,
-                'definitions': [{
-                    'readings': d['readings'],
-                    'readings_no_diacritics': [replace_diacritics(r) for r in d['readings']],
-                    'meanings': d['meanings'],
-                } for d in l['definitions']]
-            }, sys.stdout, ensure_ascii=False)
-            sys.stdout.write('\n')
+        for i, line in enumerate(f):
+            try:
+                l = json.loads(line)
+                json.dump({
+                    'word': l['word'],
+                    'simplified': [occ.convert(l['word'])],
+                    'source': source,
+                    'definitions': [{
+                        'readings': d['readings'],
+                        'readings_no_diacritics': [replace_diacritics(r) for r in d['readings']],
+                        'meanings': d['meanings'],
+                    } for d in l['definitions']]
+                }, sys.stdout, ensure_ascii=False)
+                sys.stdout.write('\n')
+            except Exception as e:
+                raise Exception(f'on line {i + 1} in {f.name}') from e
